e45d90927b62fc5bd08e188497a70d76af6d87b4
turner-prize <hiphoperation@hotmail.com>
2018-07-08 16:25:42 +0100

    Update app.py
    removed whitespace and unnessesary print function

--- app.py
+++ app.py
@@ -7,17 +7,15 @@ app = Flask(__name__)
 def windowEnumerationHandler(hwnd, top_windows):
 	top_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
 
-
 def VLCPlayPause():
 	results = []
 	top_windows = []
 	win32gui.EnumWindows(windowEnumerationHandler, top_windows)
 	for i in top_windows:
 		if "vlc media" in i[1].lower():
-			print (i)
 			win32gui.SetForegroundWindow(i[0])
 			win32api.keybd_event(0x20,0,)
- 
+
 @app.route("/")
 def hello():
 	VLCPlayPause()
